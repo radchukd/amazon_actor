a35a1346e038a772330608126275607818a04039
radchukd <radchuk.d@gmail.com>
2020-03-05 20:21:43 +0100

    merged into 1 crawler

--- main.js
+++ main.js
@@ -4,71 +4,84 @@ const Apify = require('apify');
 Apify.main(async () => {
     const input = await Apify.getInput();
     const { keyword } = input;
-    const sources = [`https://www.amazon.com/s?k=${keyword}`];
-    const searchList = await Apify.openRequestList('search', sources);
-    const itemQueue = await Apify.openRequestQueue();
-
-    const searchCrawler = new Apify.PuppeteerCrawler({
-        requestList: searchList,
-        maxRequestRetries: 2,
-        maxRequestsPerCrawl: 100,
-        maxConcurrency: 10,
-        handlePageFunction: async ({ page }) => {
-            await Apify.utils.enqueueLinks({
-                page,
-                requestQueue: itemQueue,
-                selector: 'div.s-search-results h2 > a',
-                pseudoUrls: [
-                    'https://www.amazon.com/[.*]',
-                ],
-            });
-        },
-        handleFailedRequestFunction: async ({ request }) => {
-            console.log(`Request ${request.url} failed too many times`);
-            await Apify.pushData({
-                '#debug': Apify.utils.createRequestDebugInfo(request),
-            });
+    const requestQueue = await Apify.openRequestQueue();
+    requestQueue.addRequest({
+        url: `https://www.amazon.com/s?k=${keyword}`,
+        userData: {
+            label: 'search',
         },
     });
 
-    const itemCrawler = new Apify.PuppeteerCrawler({
-        requestQueue: itemQueue,
+    const items = [];
+
+    const crawler = new Apify.PuppeteerCrawler({
+        requestQueue,
         maxRequestRetries: 2,
         maxRequestsPerCrawl: 100,
         maxConcurrency: 10,
         handlePageFunction: async ({ request, page, $ }) => {
-            await Apify.utils.puppeteer.injectJQuery(page);
-            const item = await page.evaluate(() => {
-                const title = $('h1#title span:first-child')
-                    .text()
-                    .trim();
-                const description = $('div#featurebullets_feature_div')
-                    .text()
-                    .replace(/(\n|\t)/gm, '')
-                    .trim();
-                const seller_name = $('div#merchant-info')
-                    .text()
-                    .match(/sold by (\w+)/i);
-                const offer = $('#buybox span.a-color-price:first-child')
-                    .text()
-                    .match(/(\$\d+(\.\d+)?)/);
-                const shipping = $('#buybox span.a-color-secondary')
-                    .text()
-                    .match(/(\$\d+(\.\d+)?|free)/);
+            const { label } = request.userData;
 
-                return {
-                    title,
-                    description,
-                    seller_name: seller_name ? seller_name[1] : '',
-                    offer: offer ? offer[1] : '',
-                    shipping: shipping ? shipping[1] : '',
-                };
-            });
-            const asin = request.url.match(/dp\/(.*)\//);
-            item.asin = asin ? asin[1] : null;
-            item.itemUrl = request.url;
+            if (label === 'search') {
+                await Apify.utils.enqueueLinks({
+                    page,
+                    requestQueue,
+                    selector: 'div.s-search-results h2 > a',
+                    pseudoUrls: [
+                        'https://www.amazon.com/[.*]',
+                    ],
+                    transformRequestFunction: (req) => {
+                        req.userData.label = 'item_page';
+                        return req;
+                    },
+                });
+            } else if (label === 'item_page') {
+                await Apify.utils.puppeteer.injectJQuery(page);
+                const item = await page.evaluate(() => {
+                    const title = $('h1#title span:first-child')
+                        .text()
+                        .trim();
+                    const description = $('div#featurebullets_feature_div')
+                        .text()
+                        .replace(/(\n|\t)/gm, '')
+                        .trim();
 
-            await Apify.pushData(item);
+                    return {
+                        title,
+                        description,
+                    };
+                });
+                let asin = request.url.match(/dp\/(.*)\//);
+                asin = asin ? asin[1] : null;
+                item.itemUrl = request.url;
+                item.keyword = keyword;
+
+                requestQueue.addRequest({
+                    url: `https://www.amazon.com/gp/offer-listing/${asin}`,
+                    userData: {
+                        label: 'item_offers',
+                        item,
+                    },
+                });
+            } else if (label === 'item_offers') {
+                await Apify.utils.puppeteer.injectJQuery(page);
+                const offers = await page.evaluate(() => {
+                    const parsed = [];
+                    $('div#olpOfferList div.olpOffer').each((_index, offer) => {
+                        parsed.push({
+                            offer: $(offer).find('span.olpOfferPrice').text().trim(),
+                            condition: $(offer).find('span.olpCondition').text().trim(),
+                            seller_name: $(offer).find('h3.olpSellerName').text().trim(),
+                            shipping: $(offer).find('.olpShippingInfo').text().trim(),
+                        });
+                    });
+                    return parsed;
+                });
+                offers.map((item) => {
+                    Object.assign(item, request.userData.item);
+                });
+                items.push(...offers);
+            }
         },
         handleFailedRequestFunction: async ({ request }) => {
             console.log(`Request ${request.url} failed too many times`);
@@ -77,7 +90,8 @@ Apify.main(async () => {
             });
         },
     });
-
-    await searchCrawler.run();
-    await itemCrawler.run();
+    await crawler.run();
+    await Apify.pushData({
+        items,
+    });
 });
